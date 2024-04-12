d1363206f060a4a7fa6a1b8f115f2e60ae90ee63
toratapp <73744488+toratapp@users.noreply.github.com>
2024-04-12 21:34:45 +0200

    daisy ui setup and tailwind colours customizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Button } from 'react-daisyui'
 
 function App() {
   return (
     <>
       <h1>Project exam 2</h1>
+      
+      <Button color="primary">Click me!</Button>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
+    colors: {
+      "light-beige": "#FFFBF3",
+      "burgundy": "#540D2A",
+      "middle-green": "#540D2A",
+      "dark-brown": "#3A3228",
+      "light-burgundy": "#992350",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light"],
+  }
 }
