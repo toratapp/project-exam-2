7e5151c70541f09f2e62a072d435366e14124f9b
toratapp <73744488+toratapp@users.noreply.github.com>
2024-04-27 14:16:14 +0200

    Adjusts comment schema to just be required

--- src/components/posts/commentSchema.js
+++ src/components/posts/commentSchema.js
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
 
 export const commentSchema = yup.object({
-  body: yup.string().min(11, "This is a required field.").required("This is a required field."),
+  body: yup.string().required("This is a required field."),
 });
