afe008c5e28f3b2923152dc2bbfb4870e525e258
toratapp <73744488+toratapp@users.noreply.github.com>
2024-04-28 22:04:57 +0200

    Adds active class on menu icon buttons, also hover effect on search button

--- src/components/common/layout/Header/index.jsx
+++ src/components/common/layout/Header/index.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Menu } from "react-daisyui";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from '/logo.svg';
 import ExtraHeaderMobile from "./ExtraHeaderMobile";
 import RegisterLoginLogout from "./RegisterLoginLogout";
@@ -9,6 +9,21 @@ import { useToken } from "../../../../stores/useUserStore";
 function Nav() {
   const name = useUserName();
   const token = useToken();
+  const location = useLocation();
+
+  const isActive = (path) => {
+    if (path === "/" && location.pathname === "/") {
+      return "active";
+    } else if (path === "/create-post" && location.pathname === "/create-post") {
+      return "active";
+    } else if (path === `/profiles/${name}` && location.pathname.includes(`/profiles/${name}`)) {
+      return "active";
+    } else if (path === "/profiles" && location.pathname === "/profiles" && !location.pathname.includes(`/profiles/${name}`)) {
+      return "active";
+    } else {
+      return "";
+    }
+  };
 
   return (
     <div>
@@ -19,18 +34,18 @@ function Nav() {
             <img src={Logo} className="logo p-0.5 md:p-0 ml-2 md:ml-4" alt="Logo" />
           </Link>
         </div>
-        {token && <div className="navbar__buttons relative block w-full md:w-80">
+        {token && <div className="navbar__buttons relative block w-full md:w-80 py-1.5">
           <Menu horizontal={true} className="flex justify-between ml-4 mr-4">
-            <Menu.Item className="nav__icon-button">
+            <Menu.Item className={`nav__icon-button ${isActive("/")}`}>
               <Link to="/"><i className="fa-solid fa-house"></i></Link>
             </Menu.Item>
-            <Menu.Item className="nav__icon-button">
+            <Menu.Item className={`nav__icon-button ${isActive("/create-post")}`}>
               <Link to="/create-post"><i className="fa-solid fa-square-plus"></i></Link>
             </Menu.Item>
-            <Menu.Item className="nav__icon-button">
+            <Menu.Item className={`nav__icon-button ${isActive(`/profiles/${name}`)}`}>
               <Link to={`/profiles/${name}`}><i className="fa-solid fa-user"></i></Link>
             </Menu.Item>
-            <Menu.Item className="nav__icon-button">
+            <Menu.Item className={`nav__icon-button ${isActive("/profiles")}`}>
               <Link to="/profiles"><i className="fa-solid fa-users"></i></Link>
             </Menu.Item>
           </Menu>
